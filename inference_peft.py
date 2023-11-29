9a08ae76118870724a3c1cc107c66bf20247c75f
Xinyi Lu <luxy@Xinyis-MacBook-Pro-2.local>
2023-11-29 16:33:25 -0500

    Added sth

--- inference_peft.py
+++ inference_peft.py
@@ -6,7 +6,7 @@ import os
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-device = "cuda"
+device = "cpu"
 model_name_or_path = "meta-llama/Llama-2-7b-hf"
 tokenizer_name_or_path = "meta-llama/Llama-2-7b-hf"
 peft_config = PromptTuningConfig(
@@ -23,7 +23,7 @@ checkpoint_name = f"{dataset_name}_{model_name_or_path}_{peft_config.peft_type}_
 )
 text_column = "Tweet text"
 label_column = "text_label"
-max_length = 128
+max_length = 64
 lr = 3e-2
 num_epochs = 50
 batch_size = 8
@@ -47,8 +47,10 @@ print(train_dataset['train'])
 # print(tokenized_test['input_ids'])
 
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, token="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")
+print(tokenizer.eos_token_id)
 if tokenizer.pad_token_id is None:
     tokenizer.pad_token_id = tokenizer.eos_token_id
+
 
 def preprocess_function(examples):
     inputs = [prefix + doc for doc in examples["question"]]
@@ -61,7 +63,7 @@ def preprocess_function(examples):
 
     for i in range(batch_size):
         sample_input_ids = model_inputs["input_ids"][i]
-        label_input_ids = labels["input_ids"][i] + [tokenizer.eos_token_id] * 5
+        label_input_ids = labels["input_ids"][i] + [tokenizer.eos_token_id]
         # print(i, sample_input_ids, label_input_ids)
         model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
         labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
@@ -104,57 +106,58 @@ test_dataset = test_dataset['train'].map(
     load_from_cache_file=False,
     desc="Running tokenizer on dataset",
 )
+print(test_dataset)
 
 train_dataloader = DataLoader(
     train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True
 )
 eval_dataloader = DataLoader(val_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
 
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path, token="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")
+model = AutoModelForCausalLM.from_pretrained("model/")
 model = get_peft_model(model, peft_config)
-print(model.print_trainable_parameters())
-
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-lr_scheduler = get_linear_schedule_with_warmup(
-    optimizer=optimizer,
-    num_warmup_steps=0,
-    num_training_steps=(len(train_dataloader) * num_epochs),
-)
-
+# print(model.print_trainable_parameters())
+#
+# optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+# lr_scheduler = get_linear_schedule_with_warmup(
+#     optimizer=optimizer,
+#     num_warmup_steps=0,
+#     num_training_steps=(len(train_dataloader) * num_epochs),
+# )
+#
 model = model.to(device)
 
-for epoch in range(num_epochs):
-    model.train()
-    total_loss = 0
-    for step, batch in enumerate(tqdm(train_dataloader)):
-        batch = {k: v.to(device) for k, v in batch.items()}
-        outputs = model(**batch)
-        loss = outputs.loss
-        total_loss += loss.detach().float()
-        loss.backward()
-        optimizer.step()
-        lr_scheduler.step()
-        optimizer.zero_grad()
-
-    model.eval()
-    eval_loss = 0
-    eval_preds = []
-    for step, batch in enumerate(tqdm(eval_dataloader)):
-        batch = {k: v.to(device) for k, v in batch.items()}
-        with torch.no_grad():
-            outputs = model(**batch)
-        loss = outputs.loss
-        eval_loss += loss.detach().float()
-        eval_preds.extend(
-            tokenizer.batch_decode(torch.argmax(outputs.logits, -1).detach().cpu().numpy(), skip_special_tokens=False)
-        )
-    print(eval_preds)
-    eval_epoch_loss = eval_loss / len(eval_dataloader)
-    eval_ppl = torch.exp(eval_epoch_loss)
-    train_epoch_loss = total_loss / len(train_dataloader)
-    train_ppl = torch.exp(train_epoch_loss)
-    print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
-model.save_pretrained("model/", from_pt=True)
+# for epoch in range(num_epochs):
+#     model.train()
+#     total_loss = 0
+#     for step, batch in enumerate(tqdm(train_dataloader)):
+#         batch = {k: v.to(device) for k, v in batch.items()}
+#         outputs = model(**batch)
+#         loss = outputs.loss
+#         total_loss += loss.detach().float()
+#         loss.backward()
+#         optimizer.step()
+#         lr_scheduler.step()
+#         optimizer.zero_grad()
+#
+#     model.eval()
+#     eval_loss = 0
+#     eval_preds = []
+#     for step, batch in enumerate(tqdm(eval_dataloader)):
+#         batch = {k: v.to(device) for k, v in batch.items()}
+#         with torch.no_grad():
+#             outputs = model(**batch)
+#         loss = outputs.loss
+#         eval_loss += loss.detach().float()
+#         eval_preds.extend(
+#             tokenizer.batch_decode(torch.argmax(outputs.logits, -1).detach().cpu().numpy(), skip_special_tokens=True)
+#         )
+#
+#     eval_epoch_loss = eval_loss / len(eval_dataloader)
+#     eval_ppl = torch.exp(eval_epoch_loss)
+#     train_epoch_loss = total_loss / len(train_dataloader)
+#     train_ppl = torch.exp(train_epoch_loss)
+#     print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
+# model.save_pretrained("/model", from_pt=True)
 
 with torch.no_grad():
     # inputs = {k: v.to(device) for k, v in test_dataset.items()}
@@ -168,15 +171,15 @@ with torch.no_grad():
         pred = torch.tensor([test_dataset["input_ids"][i]], device=device)
         print(pred)
         print(torch.tensor([test_dataset["attention_mask"][i]]))
-        prediction = model.generate(input_ids=pred, attention_mask=torch.tensor([test_dataset["attention_mask"][i]], device=device), max_new_tokens=30)
+        prediction = model.generate(input_ids=pred, attention_mask=torch.tensor([test_dataset["attention_mask"][i]], device=device), max_new_tokens = 30)
         decoded_pred = tokenizer.batch_decode(prediction, skip_special_tokens=True)
-# decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        # decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-# rougeLSum expects newline after each sentence
+        # rougeLSum expects newline after each sentence
         print(decoded_pred)
         decoded_preds.append("\n".join(decoded_pred))
-# decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+        # decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in d$
 
-# write to file
+        # write to file
     with open("./results/decoded_preds_peft.txt", "w") as f:
         f.write("\n".join(decoded_preds))
